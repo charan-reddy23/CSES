351ac4691a94443f05efaf74ecf3408884926bde
CHARAN REDDY <charanreddy.puchala@gmail.com>
2025-02-09 07:45:25 +0530

    Update 02. Aparments.cpp

--- Searching and Sorting/02. Aparments.cpp
+++ Searching and Sorting/02. Aparments.cpp
@@ -11,11 +11,6 @@ ostream &operator<<(ostream &ostream, vector<T> &v){for (auto &it : v) cout << i
 int main() {
     ios_base::sync_with_stdio(false); cin.tie(NULL);
 
-    #ifndef ONLINE_JUDGE
-        freopen("input.txt", "r", stdin);
-        freopen("output.txt", "w", stdout);
-    #endif
-
     int tt = 1;// cin >> tt;
 
     while (tt--){ 
