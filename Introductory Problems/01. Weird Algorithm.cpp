4b45be93937b933102cc92b104cf682e1d2fded5
CHARAN REDDY <charanreddy.puchala@gmail.com>
2025-02-09 07:46:06 +0530

    Update 01. Weird Algorithm.cpp

--- Introductory Problems/01. Weird Algorithm.cpp
+++ Introductory Problems/01. Weird Algorithm.cpp
@@ -5,11 +5,6 @@ using namespace std;
 int main() {
     ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
  
-    #ifndef ONLINE_JUDGE
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
-    #endif
-    
     int n; cin >> n;
     cout << n << " ";
     while(n > 1){
